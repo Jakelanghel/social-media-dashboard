7cfdbf5234ab54042955365cc5cd8a50e04fc626
Jake <jacobtlanghel@gmail.com>
2023-03-05 00:05:28 -0500

    init

---
+++ src/data/socialData.js
@@ -0,0 +1,32 @@
+export const socialData = {
+  username: "nathanf",
+
+  platforms: [
+    {
+      id: 1,
+      platform: "facebook",
+      followers: 1987,
+      today: 12,
+      analytics: { views: 87, viewsPer: 3, likes: 52, likesPer: -2 },
+    },
+    {
+      id: 1,
+      platform: "twitter",
+      followers: 1044,
+      today: 12,
+      analytics: { views: 117, viewsPer: 303, likes: 507, likesPer: 553 },
+    },
+    {
+      id: 1,
+      platform: "instagram",
+      followers: 1987,
+      analytics: { views: 5462, viewsPer: 2257, likes: 52000, likesPer: 1375 },
+    },
+    {
+      id: 1,
+      platform: "youtube",
+      followers: 1987,
+      analytics: { views: 107, viewsPer: 19, likes: 1407, likesPer: 12 },
+    },
+  ],
+};
